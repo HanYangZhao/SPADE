fa89ae4b7171428bcdbdcd33b70913ec5798f8c5
Han Zhao <hanzhao>
2020-08-21 16:07:30 -0400

    fixed rgb bgr error

--- test_image.py
+++ test_image.py
@@ -79,7 +79,8 @@ def main():
         generated_image = util.tensor2im(generated_image)
         generated_image_path_ = opt.output_dir + '/' + os.path.splitext(os.path.basename(opt.test_file_path))[0] +"_generated"+".png"
         print('---- generated image ', generated_image_path_, np.shape(generated_image))
-        cv2.imwrite(generated_image_path_, generated_image)
+        im_rgb = cv2.cvtColor(generated_image, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(generated_image_path_, im_rgb)
 
 if __name__ == '__main__':
     main()
